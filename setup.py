3d5885196d1c35aaccb2b03f91e3500af6fe45e6
Kot <86029286+SKbarbon@users.noreply.github.com>
2023-05-08 00:04:59 +0300

    edit

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='fpyo2ipa',
-    version='1.2',
+    name='fpyo2apk',
+    version='1.0',
     author='SKbarbon',
-    description='A package tool that allow you to built a python iOS apps with flet UI.',
-    long_description="https://github.com/SKbarbon/fpyo2ipa",
-    url='https://github.com/SKbarbon/fpyo2ipa',
+    description='A package tool that allow you to built a python Android apps with flet-pyodide dist.',
+    long_description="https://github.com/SKbarbon/fpyo2apk",
+    url='https://github.com/SKbarbon/fpyo2apk',
     install_requires=["briefcase==0.3.14"],
     include_package_data=True,
     packages=find_packages(),
